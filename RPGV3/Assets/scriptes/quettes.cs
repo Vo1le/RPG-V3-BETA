2c42c153bd78fdd7e097c1eb6914ef82dd180011
Vo1le <82891603+Vo1le@users.noreply.github.com>
2022-04-10 11:12:02 +0200

    up

---
+++ RPGV3/Assets/scriptes/quettes.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class quettes : MonoBehaviour
+{
+    //Detect collisions between the GameObjects with Colliders attached
+    void OnCollisionEnter(Collision collision)
+    {
+        //Check for a match with the specified name on any GameObject that collides with your GameObject
+        if (collision.gameObject.name == "Player")
+        {
+            //If the GameObject's name matches the one you suggest, output this message in the console
+            Debug.Log("Do something here");
+        }
+    }
+}
